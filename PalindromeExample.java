8e013c3da9a1a41a564fdf334ac5f4fc453d1141
PurvaPimpalkar <163665948+PurvaPimpalkar@users.noreply.github.com>
2025-07-15 23:48:10 +0530

    Add files via upload

---
+++ PalindromeExample.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+ class PalindromeExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        char[] arr = input.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
---
+++ ReverseString.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        char[] arr = input.toCharArray();
+        
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+         
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        
+        
+        String reversed = new String(arr);
+        
+        System.out.println("Reversed string: " + reversed);
+    }
+}
