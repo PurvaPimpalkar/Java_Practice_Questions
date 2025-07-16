5f491fa6826511c8d54e2974ca97b65129de2ddd
PurvaPimpalkar <163665948+PurvaPimpalkar@users.noreply.github.com>
2025-07-16 20:19:59 +0530

    Update and rename VowelConsonant.java to Day1_VowelConsonant.java

--- Day1_VowelConsonant.java
+++ Day1_VowelConsonant.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VowelConsonant {
+public class Day1_VowelConsonant {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
