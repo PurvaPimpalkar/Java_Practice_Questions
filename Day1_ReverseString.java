c6a9827cfb28037559582405f24453dc784eed2d
PurvaPimpalkar <163665948+PurvaPimpalkar@users.noreply.github.com>
2025-07-16 20:19:33 +0530

    Update and rename ReverseString.java to Day1_ReverseString.java

--- Day1_ReverseString.java
+++ Day1_ReverseString.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- class ReverseString {
+ class Day1_ReverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
